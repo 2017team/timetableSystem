4466d58e85cf90c012d5d61e9a2f914a0f285a8a
chaoweb <1648907448@qq.com>
2017-06-09 21:06:09 +0800

    index

--- application/poindex/controller/IndexController.php
+++ application/poindex/controller/IndexController.php
@@ -1,1 +1,13 @@
-hello world
+<?php
+namespace app\poindex\controller;
+use think\Controller;
+/**
+* 
+*/
+class indexController extends Controller
+{
+	public function index()
+	{
+		return "hello 朴世超";
+	}
+}
