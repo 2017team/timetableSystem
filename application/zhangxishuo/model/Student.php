ccc2fae9e8381cb80fbe5c9555e11dbed2802537
zxishuo <zxishuo@outlook.com>
2017-06-10 22:08:18 +0800

    2017.6.10

--- application/zhangxishuo/model/Student.php
+++ application/zhangxishuo/model/Student.php
@@ -3,6 +3,7 @@ namespace app\zhangxishuo\model;
 use think\Model;
 
 class Student extends Model{
+	// 在C层中没有调用getSexAttr()方法，怎么将sex赋值为男/女的？这个方法怎么运行的？get...Attr()是规定好的名称吗？
 	public function getSexAttr($value){
 		$status = array('0'=>'男','1'=>'女');
 		$sex = $status[$value];
@@ -11,5 +12,19 @@ class Student extends Model{
 		} else {
 			return $status[0];
 		}
+	}
+
+	protected $type = [
+		'create_time' => 'datetime',
+	];
+
+	protected $dateFormat = 'Y年m月d日';
+
+	public function getCreateTimeAttr($value){
+		return date('Y-m-d',$value);
+	}
+
+	public function Klass(){
+		return $this->belongsTo('Klass');
 	}
 }
