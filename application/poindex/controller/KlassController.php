dbf46a69e120e050de61ff0f2ad6b3785e790393
chaoweb <1648907448@qq.com>
2017-06-12 22:26:42 +0800

    poshichao 6.3

--- application/poindex/controller/KlassController.php
+++ application/poindex/controller/KlassController.php
@@ -1,18 +1,53 @@
 <?php
 namespace app\poindex\controller;
-use app\poindex\common\model\Klass;        // 班级
+use app\poindex\common\model\Klass;     // 班级
+use think\Request;       //请求
 
 class KlassController extends IndexController
 {
     public function index()
     {
-    	//获取班级名称
-    	$name = Request::instance()->get('name');
-    	echo $name;
-    	return $this->get('error');
+    	
+        try {
+            //获取班级名称
+    	    $name = input('get.name');
+            
+            //实例化klass对象
+            $Klass = new Klass;
 
-        $klasses = Klass::paginate();
-        $this->assign('klasses', $klasses);
+            // 按条件查询数据并调用分页
+            $klasses = $Klass->where('name', 'like', '%' . $name . '%')->paginate('5', false, [
+                'query'=>[
+                    'name' => $name,
+                    ],
+                ]);
+
+            // 向V层传数据
+            $this->assign('klasses', $klasses);
+
+            // 取回打包后的数据
+            $htmls = $this->fetch();
+
+            //返还给用户
+            return $htmls;
+
+            $klasses = Klass::paginate();
+            $this->assign('klasses', $klasses);
+            return $this->fetch();
+
+            
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+     public function add()
+    {
         return $this->fetch();
+    }
+
+    public function save()
+    {
+        var_dump(Request::instance()->post());
     }
 }
