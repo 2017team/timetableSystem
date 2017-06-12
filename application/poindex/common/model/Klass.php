7a66615ec989d8df0da1515cf32847d1bcd335c1
chaoweb <1648907448@qq.com>
2017-06-12 21:34:50 +0800

    修改

--- application/poindex/common/model/Klass.php
+++ application/poindex/common/model/Klass.php
@@ -7,13 +7,13 @@ use think\Model;
 class Klass extends Model
 {
 	private $Teacher;
-<<<<<<< HEAD:application/poindex/common/model/Klass.php
-	
-	/**
-	 * 获取对应的教师（辅导员）信息
-	 * @return  Teacher 教师
-	 */
-	public function getTeacher()
+
+    /**
+     * 获取对应的教师（辅导员）信息
+     * @return Teacher 教师
+     * @author <panjie@yunzhiclub.com> http://www.mengyunzhi.com
+     */
+    public function getTeacher()
     {
         if (is_null($this->Teacher)) {
             echo '执行1次 <br />';
@@ -21,10 +21,5 @@ class Klass extends Model
             $this->Teacher = Teacher::get($teacherId);
         }
         return $this->Teacher;
-=======
-
-    public function Teacher(){
-    	return $this->belongsTo('Teacher');
->>>>>>> d98fdb622b4058fbe0dc0b1cca5a2c63423751f5:application/zhangxishuo/model/Klass.php
     }
 }
--- application/poindex/controller/KlassController.php
+++ application/poindex/controller/KlassController.php
@@ -6,6 +6,11 @@ class KlassController extends IndexController
 {
     public function index()
     {
+    	//获取班级名称
+    	$name = Request::instance()->get('name');
+    	echo $name;
+    	return $this->get('error');
+
         $klasses = Klass::paginate();
         $this->assign('klasses', $klasses);
         return $this->fetch();
